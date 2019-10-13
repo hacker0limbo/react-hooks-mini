fa307cac5ab3f2b9ee5b766102adb52a45eaf9b8
Stephen <stephen.yin@outlook.com>
2019-10-13 23:47:34 +1000

    完成基本功能

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-hooks-mini
-原生 js 实现的简易 react-hooks
+
+原生 js 实现的简易 react-hooks, 仅适用于 node, 暂无和 dom 的集成
+
+示例代码查看 [example](/example.js)
---
+++ example.js
@@ -0,0 +1,74 @@
+const { useState, useEffect, render } = require('./lib')
+
+// 自定义 hooks
+// 该 hooks 接受初始 state 为一个 url 字符串, 返回的 state 为一个数组, 带有解析的 url 元素
+const useSplitURL = urlStr => {
+  const [url, setUrl] = useState(urlStr)
+  return [url.split('.'), setUrl]
+}
+
+const MyApp = () => {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState('foo')
+  const [splitedUrl, setSplitedUrl] = useSplitURL('www.reactjs.org')
+
+  useEffect(() => {
+    console.log('effect', count, text, splitedUrl)
+  }, [count, text, splitedUrl])
+
+  const clickCallback = () => {
+    setCount(prevCount => prevCount+1)
+  }
+
+  const click = () => {
+    setCount(count+1)
+  }
+
+  const type = newText => {
+    setText(newText)
+  }
+
+  const keep = () => {
+    setText(text)
+    setCount(count)
+  }
+
+  const locate = newUrl => {
+    setSplitedUrl(newUrl)
+  }
+
+  const render = () => {
+    console.log('render', { count, text, splitedUrl })
+  }
+
+  return {
+    click,
+    clickCallback,
+    type,
+    keep,
+    locate,
+    render
+  }
+}
+
+let App = render(MyApp)
+
+// click() 由于设置的是常量, 重复设置了两次, 相当于只设置了一次
+// clickCallback() 接受的是 callback, 可以正确拿到上次的 state, 再上次基础上计算
+// 最后 count 的值为 3
+App.click()
+App.click()
+App.clickCallback()
+App.clickCallback()
+App = render(MyApp)
+
+App.type('bar')
+App = render(MyApp)
+
+// 这里不会 run effect, 由于 dep array 没有任何变化
+App.keep()
+App = render(MyApp)
+
+App.locate('www.redux.js.org')
+App = render(MyApp)
+
